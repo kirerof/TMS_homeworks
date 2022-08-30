62d4c7529a627ee4597fbcb98f295ef494fff076
Kiryl Yerafeyeu <kirill.erofeev.07@gmail.com>
2022-08-30 08:16:38 -0700

    add more cpu info, add screenshot_homework1.png

--- homework1.py
+++ homework1.py
@@ -2,10 +2,25 @@ import psutil
 
 
 def cpu_info():
-    cpu_result = {}
-    cpu_data = psutil.cpu_freq()
-    cpu_result.update(current_freq=cpu_data.current)
-    return cpu_result
+    result_cpu = {}
+    freq_cpu = psutil.cpu_freq()
+    result_cpu.update(current_freq=freq_cpu.current)
+
+    count_cpu = psutil.cpu_count()
+    result_cpu.update(count_cpu=count_cpu)
+
+    percent_cpu = psutil.cpu_percent(interval=1, percpu=True)
+    even_cpu = {
+        'cpu' + str(key): val for key in range(1, psutil.cpu_count(logical=True) + 1) for val in percent_cpu
+    }
+    result_cpu.update(even_cpu)
+
+    times_cpu = psutil.cpu_times()
+    result_cpu.update(user_time=times_cpu.user,
+                      system_time=times_cpu.system,
+                      idle_time=times_cpu.idle)
+
+    return result_cpu
 
 
 def memory_info():
@@ -15,6 +30,7 @@ def memory_info():
                          available=memory_data.available,
                          percent=memory_data.percent,
                          free=memory_data.free)
+
     return memory_result
 
 
@@ -25,6 +41,7 @@ def process_info():
                           exe=process_data.exe(),
                           status=process_data.status(),
                           create_time=process_data.create_time())
+
     return process_result
 
 
@@ -35,19 +52,28 @@ def disk_info():
                        used=disk_data.used,
                        free=disk_data.free,
                        percent=disk_data.percent)
+
     return disk_result
 
 
 def show(cpu, memory, process, disk):
-    cpu_template = 'текущая частота процессора: {current_freq} Гц'
+    cpu_template = ('текущая частота процессора: {current_freq} Гц \n'
+                    'количество используемых процессоров: {count_cpu} \n'
+                    'проценты использования процессоров: {cpu1}%, {cpu2}% \n'
+                    'время, затрачиваемое процессами в пользовательском режиме: {user_time} с \n'
+                    'время, затрачиваемое процессами в режиме ядра: {system_time} с \n'
+                    'время простоя: {idle_time} с')
+
     memory_template = ('общая память: {total_memory} б \n'
                        'доступная память: {available} б \n'
                        'процент использования памяти:{percent}% \n'
                        'свободная память: {free} б')
+
     process_template = ('имя процесса: {name} \n'
                         'путь к исполняемому файлу: {exe} \n'
                         'статус процесса: {status} \n'
                         'время создания процесса: {create_time} мс')
+
     disk_template = ('информация об общей памяти диска: {total} б \n'
                      'информация об использованной памяти диска: {used} б \n'
                      'информация о свободной памяти диска: {free} б \n'
